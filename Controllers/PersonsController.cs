97876d73a4754801097f486405d00b60888a2f9e
Alexander Groisman <Alex.Groisman@ibm.com>
2021-01-29 14:30:08 -0800

    first releasse

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SampleDBConn.Model;
+using WebApplication5.Data;
+
+namespace WebApplication5.Controllers
+{
+    public class PersonsController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: Persons
+        public ActionResult Index()
+        {
+            return View(db.Persons.ToList());
+        }
+
+        // GET: Persons/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persons persons = db.Persons.Find(id);
+            if (persons == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persons);
+        }
+
+        // GET: Persons/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Persons/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,LastName,FirstName,Age")] Persons persons)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Persons.Add(persons);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(persons);
+        }
+
+        // GET: Persons/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persons persons = db.Persons.Find(id);
+            if (persons == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persons);
+        }
+
+        // POST: Persons/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,LastName,FirstName,Age")] Persons persons)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(persons).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(persons);
+        }
+
+        // GET: Persons/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persons persons = db.Persons.Find(id);
+            if (persons == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persons);
+        }
+
+        // POST: Persons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Persons persons = db.Persons.Find(id);
+            db.Persons.Remove(persons);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using SampleDBConn.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebApplication5.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> City(string city)
+        {
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+                    var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid=4e10532cbf8d78b102818e29b2cb93a5&units=metric");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<RootObject>(stringResult);
+                    var test = (new
+                    {
+                        Temp = string.Join("", rawWeather.weather.Select(x => x.description)),
+                        Summary = string.Join(",", rawWeather.weather.Select(x => x.main)),
+                        City = rawWeather.name,
+                        Country = rawWeather.sys.country
+                    });
+                    ViewBag.Temp = test.Temp;
+                    ViewBag.Summary = test.Summary;
+                    ViewBag.City = test.City;
+                    ViewBag.Country = test.Country;
+                    return View();
+
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                }
+            }
+        }
+
+        private ActionResult BadRequest(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
